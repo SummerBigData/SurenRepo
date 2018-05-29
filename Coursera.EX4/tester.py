2e39bfe18a873359ba767119d56ee7853a3ffd38
sGourapura <gourapura.2@osu.edu>
2018-05-29 13:01:35 -0400

    Possibly running ex.4 Coursera Code??

---
+++ Coursera.EX4/tester.py
@@ -0,0 +1,25 @@
+
+import numpy as np
+
+def Linearize(a, b):
+	return np.concatenate((np.ravel(a), np.ravel(b)))
+
+def Unlinearize(vec, a1, a2, b1, b2):
+	a = vec[0:a1*a2]
+	b = vec[a1*a2:len(vec)]
+	a = np.reshape(a, (a1, a2))
+	b = np.reshape(b, (b1, b2))
+	return a, b
+
+
+yarr = np.asarray([[i+3*j for i in range(4)] for j in range(7)])
+#print yarr
+
+hypo = np.asarray([[i+8*j for i in range(8)] for j in range(4)])
+#print hypo
+arr = np.asarray([1,3,5,6,2,4,6])
+vec = Linearize(yarr, hypo)
+print vec
+a, b = Unlinearize(vec, 7, 4, 4, 8)
+print a, yarr
+print b, hypo

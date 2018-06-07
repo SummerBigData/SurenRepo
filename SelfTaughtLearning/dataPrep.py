4485d5e390700800e5050b133de98f9fc5a2ab50
sGourapura <gourapura.2@osu.edu>
2018-06-07 17:21:46 -0400

    Time to move to oakley for this stuff

---
+++ SelfTaughtLearning/dataPrep.py
@@ -0,0 +1,98 @@
+# Written by: 	Suren Gourapura
+# Written on: 	June 7, 2018
+# Purpose: 	To write a Self-Taught Learning Algorithim using MNIST dataset
+# Source:	Following directions from: http://deeplearning.stanford.edu/wiki/index.php/Exercise:Self-Taught_Learning
+# Goal:		Python code to format data into 0-4 and 5-9
+
+
+import numpy as np
+import scipy.io
+import struct as st
+import gzip
+
+#---------GLOBAL VARIABLES----------GLOBAL VARIABLES----------GLOBAL VARIABLES----------GLOBAL VARIABLES
+
+# Save names for the data and labels
+save04N = 'data/60knum0-4.out'
+save59N = 'data/60knum5-9.out'
+
+save04L = 'data/60klab0-4.out'
+save59L = 'data/60klab5-9.out'
+
+#----------DEFINITIONS HERE----------DEFINITIONS HERE----------DEFINITIONS HERE----------DEFINITIONS HERE
+
+
+# Save the WAll values
+def saveW(saveStr, vec):
+	np.savetxt(saveStr, vec, delimiter=',')
+
+# Read the MNIST dataset
+def read_idx(filename, n=None):
+	with gzip.open(filename) as f:
+		zero, dtype, dims = st.unpack('>HBB', f.read(4))
+		shape = tuple(st.unpack('>I', f.read(4))[0] for d in range(dims))
+		arr = np.fromstring(f.read(), dtype=np.uint8).reshape(shape)
+		if not n is None:
+			arr = arr[:n]
+		return arr
+
+def col(matrix, i):
+    return np.asarray([row[i] for row in matrix])
+
+
+
+
+#----------STARTS HERE----------STARTS HERE----------STARTS HERE----------STARTS HERE
+
+
+
+# DATA PROCESSING
+def PrepData(string):
+	# Obtain the data values and convert them from arrays to lists
+	datx = read_idx('data/train-images-idx3-ubyte.gz', 60000)
+	daty = read_idx('data/train-labels-idx1-ubyte.gz', 60000)
+
+	# Get the data in matrix form
+	datx = np.ravel(datx).reshape((60000, 784))
+
+	# Stick the data and labels together for now
+	dat = np.hstack((daty.reshape(60000, 1), datx))
+
+	# Organize the data with respect to the labels
+
+	ind = np.argsort( dat[:,0] ).astype(int)
+	ordDat = np.zeros(dat.shape)
+	for i in range(len(ind)):
+		ordDat[i] = dat[ind[i]]
+	#ordDat = dat[dat[:,0].argsort()]
+
+	# Find the index of the last 4. For some reason, this is a 1 element array still, so we choose the only element [0]
+	last4Ind = np.argwhere(col(ordDat, 0)==4)[-1][0]
+	# Seperate the data
+	dat04 = ordDat[0:last4Ind+1]
+	dat59 = ordDat[last4Ind+1: ]
+
+	# Reorder the data
+	np.random.shuffle(dat04)
+	np.random.shuffle(dat59)
+
+	if string == '04':
+		return dat04[:,1:]/255.0, col(dat04, 0)
+	elif string == '59':
+		return dat59[:,1:]/255.0, col(dat59, 0)
+	else:
+		print 'Error, input is not "04" or "59"'
+#	# Unravel and seperate data
+#	x04 = np.ravel(dat04[1:])
+#	x59 = np.ravel()
+#	y04 = np.ravel(col(dat04, 0))
+#	y59 = np.ravel()
+
+#	# Save the data
+#	print "Saving seperated data with shapes:", x04.shape, x59.shape, y04.shape, y59.shape
+#	saveW(save04N, x04)
+#	saveW(save59N, x59)
+#	saveW(save04L, y04)
+#	saveW(save59L, y59)
+
+
---
+++ SelfTaughtLearning/stlAETester.py
@@ -0,0 +1,188 @@
+# Written by: 	Suren Gourapura
+# Written on: 	June 6, 2018
+# Purpose: 	To write a Sparce Auto-Encoder following directions from: http://deeplearning.stanford.edu/wiki/index.php/Exercise:Sparse_Autoencoder
+# Goal:		Python code to calculate probabilities from W values
+
+# Import the modules
+import numpy as np
+from numpy import genfromtxt
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+#import scipy.io
+import argparse
+import dataPrep
+
+
+
+#---------GLOBAL VARIABLES----------GLOBAL VARIABLES----------GLOBAL VARIABLES----------GLOBAL VARIABLES
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("m", help="Number of Datapoints, usually 30596", type=int)
+#parser.add_argument("f1", help="Number of Features (pixels) in images", type=int)
+#parser.add_argument("f2", help="Number of Features in hidden layer", type=int)
+parser.add_argument("lamb", help="Lambda, the overfitting knob", type=float)
+#parser.add_argument("beta", help="Beta, sparsity knob", type=float)
+#parser.add_argument("eps", help="Bounds for theta matrix randomization, [-eps, eps]", type=float)
+parser.add_argument("tolexp", help="Exponent of tolerance of minimize function, good value 10e-4, so -4", type=int)
+
+g = parser.parse_args()
+
+#g.m = 0 # Will be adjusted later
+gStep = 0
+g.eps = 0.12
+g.f1 = 784
+g.f2 = 200
+g.rho = 0.01
+g.beta = 3
+
+print 'You have chosen:', g
+print ' '
+
+
+
+#----------DEFINITIONS HERE----------DEFINITIONS HERE----------DEFINITIONS HERE----------DEFINITIONS HERE
+
+
+
+# Calculate the Hypothesis (layer 3) using just layer 1.
+def ForwardProp(WAll, a1):
+	W1, W2, b1, b2 = unLinWAll(WAll)
+	# Calculate a2 (g.m x 25)
+	a2 = hypothesis(W1, b1, a1)
+	# Calculate and return the output from a2 and W2 (g.m x 64)
+	a3 = hypothesis(W2, b2, a2)
+	return a2, a3
+
+# Unlinearize: Take a vector, break it into two vectors, and roll it back up
+def unLinWAll(vec):	
+	W1 = np.asarray([vec[0			: g.f2*g.f1]])
+	W2 = np.asarray([vec[g.f2*g.f1 		: g.f2*g.f1*2]])
+	b1 = np.asarray([vec[g.f2*g.f1*2 	: g.f2*g.f1*2 + g.f2]])
+	b2 = np.asarray([vec[ g.f2*g.f1*2 + g.f2 : g.f2*g.f1*2 + g.f2 + g.f1]])
+	return W1.reshape(g.f2, g.f1) , W2.reshape(g.f1, g.f2), b1.reshape(g.f2, 1), b2.reshape(g.f1, 1)
+
+def PlotImg(mat):
+	imgplot = plt.imshow(mat, cmap="binary", interpolation='none') 
+	plt.show()
+
+# Calculate the Hypothesis (for layer l to l+1)
+def hypothesis(W, b, dat):
+	oldhypo = np.matmul(W, dat.T) + b
+	oldhypo = np.array(oldhypo, dtype=np.float128)	# Helps prevent overflow errors
+	newhypo = 1.0/(1.0+np.exp(-oldhypo))	
+	return np.array(newhypo.T, dtype=np.float64)
+
+def Norm(mat):
+	Min = np.amin(mat)
+	Max = np.amax(mat)
+	nMin = 0
+	nMax = 1
+	return ((mat - Min) / (Max - Min)) * (nMax - nMin) + nMin
+
+#----------STARTS HERE----------STARTS HERE----------STARTS HERE----------STARTS HERE
+
+
+
+# DATA PROCESSING
+
+# Get data. Call the data by acccessing the function in dataPrep
+dat, y = dataPrep.PrepData('04')
+a1 = dat[:g.m, :]
+y = y[:g.m]
+
+# Obtain the best theta values from the text file
+bestWAll = np.genfromtxt(saveStr, dtype=float)
+
+
+# FORWARD PROPAGATE AND CALCULATE BEST GUESSES
+# Feed the best W and b vals into forward propagation
+a2, a3 = ForwardProp(bestWAll, a1)
+
+for i in range(g.m):
+	a2[i] = Norm(a2[i])
+	a3[i] = Norm(a3[i])
+
+
+# PROBABILITIES
+prob = np.zeros((g.m, g.f1))
+for i in range(g.m):
+	prob[i] = np.abs(a1[i]-a3[i])
+
+print 'The average seperation between a1 and a3 is (Note: 0-1, where 0 is close)', np.mean(prob)
+
+
+
+
+
+
+
+
+
+# SHOW IMAGES
+hspaceAll = np.asarray([ [0 for i in range(53)] for j in range(5)])
+picAll = hspaceAll
+
+for i in range(10):
+	# Store the pictures
+	picA1 = np.reshape(np.ravel(a1[i*100]), (8,8))
+	picA2 = np.reshape(np.ravel(a2[i*100]), (5,5))
+	picA3 = np.reshape(np.ravel(a3[i*100]), (8,8))
+#	print np.linalg.norm(a1[i*100])
+#	print np.linalg.norm(a3[i*100])
+	# DISPLAY PICTURES
+	# To display a2 in revprop, a1, and a2 in forward prop, we design some spaces
+	hspace = np.asarray([ [0 for i in range(8)] for j in range(8)])
+	vspace1 = np.asarray([ [0 for i in range(5)] for j in range(1)])
+	vspace2 = np.asarray([ [0 for i in range(5)] for j in range(2)])
+
+	# We stitch the vertical spaces onto the pictures
+	picA2All = np.concatenate((vspace1, picA2, vspace2), axis = 0)
+	# We stitch the horizontal pictures together
+	picAlli = np.concatenate((hspace, picA1, hspace, picA2All, hspace, picA3, hspace), axis = 1)
+	# Finally, add this to the picAll
+	picAll = np.vstack((picAll, picAlli, hspaceAll))
+
+# Display the pictures
+imgplot = plt.imshow(picAll, cmap="binary", interpolation='none') 
+plt.savefig('results/a123'+'Tol'+str(g.tolexp)+'Lamb'+str(g.lamb)+'rand'+g.randData+'.png',transparent=False, format='png')
+plt.show()
+
+
+
+# We also want a picture of the activations for each node in the hidden layer
+W1, W2, b1, b2 = unLinWAll(bestWAll)
+W1Len = np.sum(W1**2)**(-0.5)
+X = W1 / W1Len			# (25 x 64)
+X = Norm(X)
+
+picX = np.zeros((25,8,8))
+for i in range(25):
+	picX[i] = np.reshape(np.ravel(X[i]), (8,8))
+
+hblack = np.asarray([ [1 for i in range(52)] for j in range(2)])
+vblack = np.asarray([ [1 for i in range(2)] for j in range(8)])
+
+picAll = hblack
+for i in range(5):
+	pici = np.concatenate((vblack, picX[5*i+0], vblack, picX[5*i+1], vblack, picX[5*i+2], vblack, picX[5*i+3], vblack, picX[5*i+4], vblack), axis = 1)
+	picAll = np.vstack((picAll, pici, hblack))
+
+# Display the pictures
+imgplot = plt.imshow(picAll, cmap="binary", interpolation='none') 
+plt.savefig('results/aHL'+'Tol'+str(g.tolexp)+'Lamb'+str(g.lamb)+'rand'+g.randData+'.png',transparent=False, format='png')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+

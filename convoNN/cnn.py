612a0d7db3749b90438eb1cef626078951e0eab5
sGourapura <gourapura.2@osu.edu>
2018-06-18 17:33:29 -0400

    Synching

--- convoNN/cnn.py
+++ convoNN/cnn.py
@@ -14,9 +14,88 @@ import argparse
 #import matplotlib.pyplot as plt
 from scipy.optimize import check_grad
 #from random import randint
-#import dataPrepColor
+import dataPrep
 from scipy.signal import convolve2d
+
+
+parser = argparse.ArgumentParser()
+#parser.add_argument("m", help="Number of Datapoints, usually 100000", type=int)
+#parser.add_argument("f1", help="Number of Features (pixels) in images", type=int)
+#parser.add_argument("f2", help="Number of Features in hidden layer", type=int)
+#parser.add_argument("lamb", help="Lambda, the overfitting knob", type=float)
+#parser.add_argument("beta", help="Beta, sparsity knob", type=float)
+#parser.add_argument("eps", help="Bounds for theta matrix randomization, [-eps, eps]", type=float)
+#parser.add_argument("tolexp", help="Exponent of tolerance of minimize function, good value 10e-4, so -4", type=int)
+
+g = parser.parse_args()
+
+
+g.f1 = 192
+g.f2 = 400
+g.m = 100000
+
+saveStr = 'data/m1000Tol-4Lamb0.003beta5.0.out'
+
+print 'You have chosen:', g
+print ' '
+
+
+
+
 
 def conv(img, mat):
 	matRevTP = np.flipud(np.fliplr(mat))
 	return convolve2d(img, matRevTP, mode='valid', boundary='fill', fillvalue=0)
+
+def sig(x):
+	return 1.0 / (1.0 + np.exp(-x))
+
+def Convolve(imgs, W, b, ZCAmat, mpatch):
+	
+	convImg = np.zeros(( ))
+	for i in range(imgs.shape[0]):			# For each image
+		for j in range(W.shape[0]):		# For each feature
+			for k in range(imgs.shape[3]):	# For each color
+				WT = W[j].dot(ZCAmat)
+				convImg = sig( WT.dot(imgs[i,:,:,k]) - WT.dot(mpatch) + b) 
+	return convImg
+
+# Unlinearize: Take a vector, break it into two vectors, and roll it back up
+def unLinWAll(vec):	
+	W1 = np.asarray([vec[0			: g.f2*g.f1]])
+	#W2 = np.asarray([vec[g.f2*g.f1 		: g.f2*g.f1*2]])
+	b1 = np.asarray([vec[g.f2*g.f1*2 	: g.f2*g.f1*2 + g.f2]])
+	#b2 = np.asarray([vec[ g.f2*g.f1*2 + g.f2 : g.f2*g.f1*2 + g.f2 + g.f1]])
+	#return W1.reshape(g.f2, g.f1) , W2.reshape(g.f1, g.f2), b1.reshape(g.f2, 1), b2.reshape(g.f1, 1)
+	return W1.reshape(g.f2, g.f1), b1.reshape(g.f2, 1)
+
+
+
+
+# DATA PROCESSING
+
+# Get data. Call the data by acccessing the function in dataPrepColor
+patches = dataPrep.GenDat()	# 100k x 64 x 3
+imgs = dataPrep.GenSubTrain()	# 2000 x 64 x 64 x 3
+
+# Whiten data
+wpatches, ZCAmat, patches, mpatch = dataPrep.SamzcaWhite(patches)
+#(100000, 192) (192, 192) (100000, 192) (1, 192)
+
+# Reorganize the mean patch as 8 x 8 x 3
+mpatch = np.ravel(mpatch)
+mSqpatch = np.zeros((8,8,3))
+mSqpatch[:,:,0] = mpatch[:64].reshape(8, 8)
+mSqpatch[:,:,1] = mpatch[64:128].reshape(8, 8)
+mSqpatch[:,:,2] = mpatch[128:].reshape(8, 8)
+
+# Get the W and b arrays
+bestWAll = np.genfromtxt(saveStr, dtype=float)
+W1, b1 = unLinWAll(bestWAll)	#(400, 192) (400, 1)
+
+print Convolve(imgs, W1, b1, ZCAmat, mSqpatch).shape
+
+
+
+
+
---
+++ convoNN/dataPrep.py
@@ -0,0 +1,172 @@
+# Written by: 	Suren Gourapura
+# Written on: 	June 12, 2018
+# Purpose: 	To write a linear Sparce Auto-Encoder following directions from: http://deeplearning.stanford.edu/wiki/index.php/Exercise:Learning_color_features_with_Sparse_Autoencoders
+# Goal:		Generate the dataset and send it to the main code
+
+
+import numpy as np
+import time
+import scipy.io
+
+#---------GLOBAL VARIABLES----------GLOBAL VARIABLES----------GLOBAL VARIABLES----------GLOBAL VARIABLES
+
+datlen = 100000
+pixlen = 64
+collen = 3
+eps = 0.1
+
+saveStr = 'data/m'+str(datlen/1000.0)+'kZCA.out'
+
+#----------DEFINITIONS HERE----------DEFINITIONS HERE----------DEFINITIONS HERE----------DEFINITIONS HERE
+
+# Save the WAll values
+def save(vec):
+	np.savetxt(saveStr, vec, delimiter=',')
+
+def PlotImg(mat):
+	import matplotlib.pyplot as plt
+	imgplot = plt.imshow(mat, cmap="binary", interpolation='none') 
+	plt.show()
+
+def col(matrix, i):
+	return np.asarray([row[i] for row in matrix])
+
+def Norm(mat):
+	Min = np.amin(mat)
+	Max = np.amax(mat)
+	nMin = 0
+	nMax = 1
+	return ((mat - Min) / (Max - Min)) * (nMax - nMin) + nMin
+
+#----------STARTS HERE----------STARTS HERE----------STARTS HERE----------STARTS HERE
+
+def GenDat():
+	# To see how long the code runs for, we start a timestamp
+	totStart = time.time()
+	print 'Running dataPrep.py, GenDat'
+
+	# Get the data. It is 192 x 100k, so we transpose into 100k x 192
+	data = scipy.io.loadmat('data/stlSampledPatches.mat')['patches'].T
+	#data = data.reshape((datlen, pixlen, collen))
+
+	# Stop the timestamp and print out the total time
+	totend = time.time()
+	print"dataPrep.py's GenDat took ", totend - totStart, 'seconds to run'
+	print ' '
+	return data
+
+
+
+def GenSubTrain():
+	# To see how long the code runs for, we start a timestamp
+	totStart = time.time()
+	print 'Running dataPrep.py, GenSubTrain'
+
+	# Get the data. It is 64 x 64 x 3 x 2000
+	data = scipy.io.loadmat('data/stlTrainSubset.mat')['trainImages']
+
+	ordDat = np.zeros((2000, 64, 64, 3))
+	for i in range(2000):
+		ordDat[i] = data[ :, :, :, i]
+
+	# Stop the timestamp and print out the total time
+	totend = time.time()
+	print"dataPrep.py's GenSubTrain took ", totend - totStart, 'seconds to run'
+	print ' '
+	return ordDat
+
+
+def GenSubTest():
+	# To see how long the code runs for, we start a timestamp
+	totStart = time.time()
+	print 'Running dataPrep.py, GenSubTest'
+
+	# Get the data. It is 64 x 64 x 3 x 3200
+	data = scipy.io.loadmat('data/stlTestSubset.mat')['testImages']
+	
+	ordDat = np.zeros((3200, 64, 64, 3))
+	for i in range(3200):
+		ordDat[i] = data[ :, :, :, i]
+
+	# Stop the timestamp and print out the total time
+	totend = time.time()
+	print"dataPrep.py's GenSubTest took ", totend - totStart, 'seconds to run'
+	print ' '
+	return ordDat
+
+
+def zcaWhite(inputs):	# To see how long the code runs for, we start a timestamp
+	totStart = time.time()
+	print 'Running dataPrepColor.py, ZCAwhite'
+	# Reshape the data into 100k x 192
+	datl = inputs.shape[0]
+	pixl = inputs.shape[1]
+	coll = inputs.shape[2]
+	inputs = inputs.reshape((datl, pixl*coll))
+
+	sigma = np.dot(inputs.T, inputs)/datl #Correlation matrix
+	U,S,V = np.linalg.svd(sigma) #Singular Value Decomposition
+        #eps is the whitening constant, it prevents division by zero
+	ZCAMatrix = np.dot(np.dot(U, np.diag(1.0/np.sqrt(S + eps))), U.T)                     
+
+	# Stop the timestamp and print out the total time
+	totend = time.time()
+	print"dataPrepColor.py's ZCAwhite took ", totend - totStart, 'seconds to run'
+	print ' '
+
+	return np.dot(inputs, ZCAMatrix), ZCAMatrix	# Return the whitened image and the matrix
+
+def SamzcaWhite(X):
+#	# Reshape the data into 100k x 192
+#	datl = X.shape[0]
+#	pixl = X.shape[1]
+#	coll = X.shape[2]
+#	X = X.reshape((datl, pixl*coll))
+
+	mu = np.mean(X, axis=0).reshape(1, X.shape[1])
+	X -= np.tile(mu, (X.shape[0], 1))
+	Sigma = np.dot(X.T, X)/X.shape[0]
+	U, S, _ = np.linalg.svd(Sigma)
+	S = S.reshape(len(S), 1)
+	l_mat = np.diag(1.0/np.sqrt(S.flatten() + eps))
+	ZCA_mat = U.dot(l_mat).dot(U.T)
+	return X.dot(ZCA_mat), ZCA_mat, X, mu
+
+
+#dat = GenDat()
+
+## Reshape and normalize the data
+##for i in range(dat.shape[0]):
+##	dat[i] = Norm(dat[i])
+
+#print np.amin(dat), np.amax(dat)
+#print np.amin(dat[1]), np.amax(dat[1])
+
+#ZCA, ZCAmat = zcaWhite(dat) # 100k x 192 and 192 x 192
+##save(np.ravel(ZCAmat))
+
+#ZCAsam, ZCAmatsam = SamzcaWhite(dat)
+
+## Reshape and normalize the data
+#for i in range(dat.shape[0]):
+#	ZCA[i] = Norm(ZCA[i])
+#	ZCAsam[i] = Norm(ZCAsam[i])
+
+#print np.amin(dat[1]), np.amax(dat[1])
+#print np.amin(ZCA[1]), np.amax(ZCA[1])
+#print np.amin(ZCAsam[1]), np.amax(ZCAsam[1])
+#ZCA = Norm(ZCA)
+
+
+#vline = np.ones((8, 1, 3))
+#hline = np.ones((1, 8*3+4, 3))
+#picAll = hline
+
+#for i in range(10):
+#	pici = np.hstack((vline, dat[i].reshape(8,8,3), vline, ZCA[i].reshape(8,8,3), vline,ZCAsam[i].reshape(8,8,3), vline))
+#	picAll = np.vstack((picAll, pici, hline))
+
+#PlotImg(picAll)
+
+
+
